385d27d20e7a37a049e897fd4c5185991a8fb15f
Dan0268 <boskovic.danijel@gmail.com>
2021-11-03 12:37:24 +1100

    projects updated

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,38 +3,43 @@ import { projectitems } from "./projectitems";
 import { Link } from "react-router-dom";
 
 const Projects = () => {
-    return (
-        <section className={styles.Projects} id="projects">
-            <div className={styles.Projects__container}>
-                <div className={styles.Projects__Title}>
-                    <h1>Projects</h1>
-                    <h2>Web Development</h2>
-                </div>
-                <div className={styles.Projects__Cards}>
-                    {projectitems.map((project) => (
-                        <div className={styles.Projects__Card}>
-                            <img src={project.image} alt={project.name} />
+  return (
+    <section className={styles.Projects} id="projects">
+      <div className={styles.Projects__container}>
+        <div className={styles.Projects__Title}>
+          <h1>Projects</h1>
+          <h2>Web Development</h2>
+        </div>
+        <div className={styles.Projects__Cards}>
+          {projectitems.map((project) => (
+            <div className={styles.Projects__Card}>
+              <img src={project.image} alt={project.name} />
 
-                            <div className={styles.Projects__Card_Details}>
-                                <h3>{project.name}</h3>
-                                {project.github ? (
-                                    <a href={project.github}>
-                                        <p>Github Link</p>
-                                    </a>
-                                ) : (
-                                    <>
-                                        <p></p>
-                                    </>
-                                )}
+              <div className={styles.Projects__Card_Details}>
+                <h3>{project.name}</h3>
+                {project.github ? (
+                  <a href={project.github}>
+                    <p>Github Link</p>
+                  </a>
+                ) : (
+                  <>{/* <p></p> */}</>
+                )}
+                {project.link ? (
+                  <a href={project.link}>
+                    <p>Link</p>
+                  </a>
+                ) : (
+                  <>{/* <p></p> */}</>
+                )}
 
-                                <p>{project.description}</p>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+                <p>{project.description}</p>
+              </div>
             </div>
-        </section>
-    );
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
--- src/components/Projects/projectitems.js
+++ src/components/Projects/projectitems.js
@@ -4,47 +4,64 @@ import googleBooks from "../../pictures/projects/google-books-search.jpg";
 import ecommerce from "../../pictures/projects/ecommerce.jpg";
 import mothdx from "../../pictures/projects/mothdx.jpg";
 import minesweeper from "../../pictures/projects/minesweeper.jpg";
+import rodrigoacuna from "../../pictures/projects/rodrigoacuna.jpg";
+import thirdeyeobservatory from "../../pictures/projects/thirdeyeobservatory.jpg";
 
 export const projectitems = [
-    {
-        image: mothdx,
-        name: "Moth Diagnostics LIMS",
-        description:
-            "A LIMS tool made for the Sydney Covid Testing startup Moth Diagnostics as part of the _nology course client project. This was a real team effort in bringing together MySQL database with Spring Boot backend application, with a frontend made in React. I learned a lot not only in terms of technical challenge but also coordination within a group.",
-    },
-    {
-        image: ecommerce,
-        name: "Ecommerce Site",
-        github: "https://github.com/Dan0268/ecommerce-project.git",
-        description:
-            "A ecommerce site from an exercise in the _nology programming course, made in React using Firebase api and storage. Modeled after my old T-Shirt shop. One that I would like to come back to and develop into a full application.",
-    },
-    {
-        image: googleBooks,
-        name: "Google Books Search",
-        github: "https://github.com/Dan0268/BookApiProject",
-        description:
-            "A website made in the _nology programming course to make searches on Google Books through their api. Made in html, scss/css and Javascript",
-    },
-    {
-        image: minesweeper,
-        name: "Minesweeper",
-        github: "https://github.com/Dan0268/Java-Party-Games",
-        description:
-            "In the _nology programming course we had to build a Minesweeper application in Java as an exercise. I enjoyed it so much that expanded on it, adding an intro and a menu allowing for other party games down the track.",
-    },
-    {
-        image: calculator,
-        name: "Online Calculator",
-        github: "https://github.com/Dan0268/Calculator",
-        description:
-            "A humble pocket calculator made in html, scss/css and Javascript. Done as part of the _nology course, I think it was our first proper exercise.",
-    },
-    {
-        image: morseCode,
-        name: "Morse Code Translator",
-        github: "https://github.com/Dan0268/Morse-Code-Translator",
-        description:
-            "A little morse code translator made in html, css and Javascript. Enter text or morse code in the top field and get a translation. Very handy if you need to send a telegram.",
-    },
+  {
+    image: rodrigoacuna,
+    name: "Rodrigo Acuña",
+    link: "https://rodrigoacuna.com/",
+    description:
+      "Redesign of a client website to make it more presentable, more manageable for posting articles and podcasts, made in Squarespace. The project also involved setting up a Patreon page, a little graphic design and custom CSS injection.",
+  },
+  {
+    image: thirdeyeobservatory,
+    name: "Third Eye Observatory",
+    link: "https://www.thirdeyeobservatory.com/",
+    description:
+      "Astronomy website based on client specification, made in Wix including custom code with OpenSeaDragon and image processing with the Zoomable plugin for Photoshop. This allows the seamless zooming in and out of the large photographs both on desktop and mobile.",
+  },
+
+  {
+    image: mothdx,
+    name: "Moth Diagnostics LIMS",
+    description:
+      "A LIMS tool made for the Sydney Covid Testing startup Moth Diagnostics as part of the _nology course client project. This was a real team effort in bringing together MySQL database with Spring Boot backend application, with a frontend made in React. I learned a lot not only in terms of technical challenge but also coordination within a group.",
+  },
+  {
+    image: ecommerce,
+    name: "Ecommerce Site",
+    github: "https://github.com/Dan0268/ecommerce-project.git",
+    description:
+      "A ecommerce site from an exercise in the _nology programming course, made in React using Firebase api and storage. Modeled after my old T-Shirt shop. One that I would like to come back to and develop into a full application.",
+  },
+  {
+    image: googleBooks,
+    name: "Google Books Search",
+    github: "https://github.com/Dan0268/BookApiProject",
+    description:
+      "A website made in the _nology programming course to make searches on Google Books through their api. Made in html, scss/css and Javascript",
+  },
+  {
+    image: minesweeper,
+    name: "Minesweeper",
+    github: "https://github.com/Dan0268/Java-Party-Games",
+    description:
+      "In the _nology programming course we had to build a Minesweeper application in Java as an exercise. I enjoyed it so much that expanded on it, adding an intro and a menu allowing for other party games down the track.",
+  },
+  {
+    image: calculator,
+    name: "Online Calculator",
+    github: "https://github.com/Dan0268/Calculator",
+    description:
+      "A humble pocket calculator made in html, scss/css and Javascript. Done as part of the _nology course, I think it was our first proper exercise.",
+  },
+  {
+    image: morseCode,
+    name: "Morse Code Translator",
+    github: "https://github.com/Dan0268/Morse-Code-Translator",
+    description:
+      "A little morse code translator made in html, css and Javascript. Enter text or morse code in the top field and get a translation. Very handy if you need to send a telegram.",
+  },
 ];
